93e09ca9aef1168ad88003ce36df1b83efdf33c6
Robin Jones <networkfusion@users.noreply.github.com>
2025-06-29 19:44:56 +0100

    Update generate_release.ps1
    Disregard media type for release folder structure.

--- generate_release.ps1
+++ generate_release.ps1
@@ -1,10 +1,16 @@
 $sourceRoot = Join-Path $PSScriptRoot 'metadata'
-$destRoot = Join-Path $PSScriptRoot 'release\metadata'
+$destRoot = Join-Path $PSScriptRoot 'release/metadata'
 
 # Find all description.txt files under metadata
 Get-ChildItem -Path $sourceRoot -Filter 'description.txt' -Recurse | ForEach-Object {
     $relativeDir = Split-Path ($_.DirectoryName.Substring($sourceRoot.Length)) -NoQualifier
-    $destDir = Join-Path $destRoot $relativeDir
+    $dirParts = $relativeDir -split '[\\/]' | Where-Object { $_ -ne '' }
+    # If the last folder is media type 'E', move up one directory (to use the USA English as a failback).
+    if ($dirParts.Count -gt 0 -and $dirParts[-1] -eq 'E') {
+        $destDir = Join-Path $destRoot ( $dirParts[0..($dirParts.Count-2)] -join '/' )
+    } else {
+        $destDir = Join-Path $destRoot $relativeDir
+    }
     if (-not (Test-Path $destDir)) {
         New-Item -Path $destDir -ItemType Directory | Out-Null
     }
