70728f67211f89949cf2cb5ec012ab48588b1210
Robin Jones <networkfusion@users.noreply.github.com>
2026-02-01 19:33:50 +0000

    Improve .meta files
    N64FlashcartMenu INI parser cannot handle spaces between name = value

---
+++ release_generate_metadata_zips.py
@@ -0,0 +1,7 @@
+import os
+import shutil
+
+source_root = os.path.join(os.path.dirname(__file__), 'release')
+dest_root = os.path.join(os.path.dirname(__file__), 'release-brew-meta', 'release')
+
+# 
